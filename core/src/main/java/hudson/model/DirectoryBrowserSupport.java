7bb2d68c9250ed582614ae97c5c32306772841bc
Tom Huybrechts <tom.huybrechts@gmail.com>
2011-12-27 10:17:08 +0100

    compressed file support for directory browser
    Conflicts:
    	core/src/main/java/hudson/model/DirectoryBrowserSupport.java

--- core/src/main/java/hudson/model/DirectoryBrowserSupport.java
+++ core/src/main/java/hudson/model/DirectoryBrowserSupport.java
@@ -40,6 +40,7 @@ import java.util.Locale;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import jenkins.model.Jenkins;
@@ -275,7 +276,13 @@ public final class DirectoryBrowserSupport implements HttpResponse {
             baseFile = baseFile.child(indexFileName);
         }
 
+		boolean compressed = false;
+		String originalName = baseFile.getName();
         //serve a single file
+		if (!baseFile.exists()) {
+			baseFile = baseFile.getParent().child(baseFile.getName() + ".gz");
+			compressed = true;
+		}
         if(!baseFile.exists()) {
             rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
@@ -289,20 +296,23 @@ public final class DirectoryBrowserSupport implements HttpResponse {
         }
 
         long lastModified = baseFile.lastModified();
-        long length = baseFile.length();
+        long length = compressed ? -1 : baseFile.length();
 
         if(LOGGER.isLoggable(Level.FINE))
             LOGGER.fine("Serving "+baseFile+" with lastModified=" + lastModified + ", length=" + length);
 
         InputStream in = baseFile.open();
+
+        if (compressed) in = new GZIPInputStream(in);
+
         if (view) {
             // for binary files, provide the file name for download
-            rsp.setHeader("Content-Disposition", "inline; filename=" + baseFile.getName());
+            rsp.setHeader("Content-Disposition", "inline; filename=" + originalName);
 
             // pseudo file name to let the Stapler set text/plain
             rsp.serveFile(req, in, lastModified, -1, length, "plain.txt");
         } else {
-            rsp.serveFile(req, in, lastModified, -1, length, baseFile.getName() );
+            rsp.serveFile(req, in, lastModified, -1, length, originalName );
         }
     }
 
